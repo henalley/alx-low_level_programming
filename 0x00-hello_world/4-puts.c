837841e427c8f8229fd48ab42ff2548bdb81692f
henalley <henryalley@hotmail.com>
2022-07-14 17:08:58 -0700

    4-puts.c answer updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'Using puts to display a string'
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	char myString[] = "\"Programming is like building a multilingual puzzle";
+	char myString[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(myString);
 
