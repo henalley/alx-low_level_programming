8d8a5a1a5746020fe2af2586b4e2fc4330ebdf73
henalley <henryalley@hotmail.com>
2022-07-14 17:26:29 -0700

    5-printf.c added with answer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include < stdio.h >
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Printing a statement using printf'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char myString[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(myString);
+
+	return (0);
+}
